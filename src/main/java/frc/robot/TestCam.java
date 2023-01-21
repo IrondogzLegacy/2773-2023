9f2c1f50de9349d8b5affbc3df5b9852175280fb
IronDogz <finndlacher@gmail.com>
2023-01-21 14:33:10 -0600

    ID Camera

--- src/main/java/frc/robot/TestCam.java
+++ src/main/java/frc/robot/TestCam.java
@@ -3,10 +3,11 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class TestCam extends SubsystemBase {
-  private final SerialPort serialPort = new SerialPort(115200, SerialPort.Port.kUSB);
+  private final SerialPort serialPort = new SerialPort(115200, SerialPort.Port.kUSB1);
   /** Creates a new TestCam. */
   public TestCam() {
 
@@ -14,8 +15,19 @@ public class TestCam extends SubsystemBase {
 
   @Override
   public void periodic() {
+    if (serialPort.getBytesReceived() > 0){
     String s = serialPort.readString();
-    System.out.println(s);
+    DriverStation.reportWarning("Data:" + s, false);
+    String[] tokens = s.split(",");    
+    for (String t : tokens)
+      System.out.println(t);
+    } else { 
+      System.out.println("test");
+    }
     // This method will be called once per scheduler run
   }
 }
+//TAG_FOUND: 1;0.516811,0.116328,-0.848159,-0.149319,0.987787,0.044494,0.842977,0.103652,0.527869;-0.487307,0.097861,1.649744;0.000010
+
+
+
--- src/main/java/frc/robot/TestRobotContainer.java
+++ src/main/java/frc/robot/TestRobotContainer.java
@@ -6,12 +6,13 @@ public class TestRobotContainer {
     private final Joystick m_stick = new Joystick(0);
     private final TestDriveSubsystem driveSubsystem = new TestDriveSubsystem();
     private final TestDriveCommand driveCommand = new TestDriveCommand(driveSubsystem, m_stick);
-
+    private final TestCam testCam = new TestCam();
     public TestRobotContainer() {
         // Configure the button bindings
         configureButtonBindings();
 
         driveSubsystem.setDefaultCommand(driveCommand);
+        testCam.setDefaultCommand(new TestCamCommand(testCam));
     }
 
     private void configureButtonBindings() {
