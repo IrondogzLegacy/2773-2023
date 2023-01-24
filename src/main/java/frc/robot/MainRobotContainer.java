904b5c7a7f65b3a0500fce206a1b03c13edff67a
Tacocat1016 <122695971+Tacocat1016@users.noreply.github.com>
2023-01-23 19:10:13 -0600

    Update MainRobotContainer.java

--- src/main/java/frc/robot/MainRobotContainer.java
+++ src/main/java/frc/robot/MainRobotContainer.java
@@ -1,12 +1,12 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
-
+//imports joystick controls and functions
 public class MainRobotContainer {
     private final Joystick m_stick = new Joystick(0);
     private final MainDriveSubsystem driveSubsystem = new MainDriveSubsystem();
     private final MainDriveCommand driveCommand = new MainDriveCommand(driveSubsystem, m_stick);
-
+//Needed to make the controller function
     public MainRobotContainer() {
         // Configure the button bindings
         configureButtonBindings();
