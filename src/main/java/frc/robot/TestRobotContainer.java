670b1a73fe1a1ce7968a4d3ba900ecfd3bf513c3
Tacocat1016 <122695971+Tacocat1016@users.noreply.github.com>
2023-01-28 12:49:45 -0600

    Update TestRobotContainer.java

--- src/main/java/frc/robot/TestRobotContainer.java
+++ src/main/java/frc/robot/TestRobotContainer.java
@@ -8,7 +8,7 @@ public class TestRobotContainer {
     private final Joystick m_stick = new Joystick(0);
     private final TestDriveSubsystem driveSubsystem = new TestDriveSubsystem();
     private final TestDriveCommand driveCommand = new TestDriveCommand(driveSubsystem, m_stick);
-    private final TestCam testCam = new TestCam();
+   // private final TestCam testCam = new TestCam();
     private final TestNavigationSubsystem navigationSubsystem = new TestNavigationSubsystem();
     private final TestEncoderSubsystem testEncoderSubsystem = new TestEncoderSubsystem();
 
@@ -38,11 +38,16 @@ public class TestRobotContainer {
     Good luck to us :P Will have to use encoders in TestEncoderSubsystem for this!
     **/
         MoveDistanceCommand move3 = new MoveDistanceCommand(testEncoderSubsystem, driveSubsystem, 3);
+        MoveDistanceCommand move3b = new MoveDistanceCommand(testEncoderSubsystem, driveSubsystem, 3);
         TestTurnAngleCommand turn1710 = new TestTurnAngleCommand(driveSubsystem, navigationSubsystem, 1710);
         MoveDistanceCommand move2 = new MoveDistanceCommand(testEncoderSubsystem, driveSubsystem, 2);
+        MoveDistanceCommand move2b = new MoveDistanceCommand(testEncoderSubsystem, driveSubsystem, 2);
         TestTurnAngleCommand turn180 = new TestTurnAngleCommand(driveSubsystem, navigationSubsystem, 180);
+        TestTurnAngleCommand turn180b = new TestTurnAngleCommand(driveSubsystem, navigationSubsystem, 180);
         TestTurnAngleCommand turn90 = new TestTurnAngleCommand(driveSubsystem, navigationSubsystem, 90);
-        return move3.andThen(turn1710).andThen(move2).andThen(turn180).andThen(move2).andThen(turn90).andThen(move3).andThen(turn180);
+        return move3.andThen(turn1710).andThen(move2)
+        .andThen(turn180).andThen(move2b).andThen(turn90)
+        .andThen(move3b).andThen(turn180b);
     }
     JoystickButton button1 = new JoystickButton(m_stick, 1);
     JoystickButton button4 = new JoystickButton(m_stick, 4);
