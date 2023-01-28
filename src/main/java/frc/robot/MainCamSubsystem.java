f05bd93cfa20232ae46a1654c1d19893c3e0f931
IronDogz <finndlacher@gmail.com>
2023-01-28 13:33:25 -0600

    Speed and Vision

---
+++ src/main/java/frc/robot/MainCamSubsystem.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.SerialPort;
+
+
+public class MainCamSubsystem {
+    private final SerialPort serialPort = new SerialPort(115200, SerialPort.Port.kMXP);
+    
+
+}
