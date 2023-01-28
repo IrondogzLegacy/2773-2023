145f277ce92e5cc9c5c1742f7299a292bdfc509b
Space-Enthusiast-Nova <96958064+Space-Enthusiast-Nova@users.noreply.github.com>
2023-01-28 11:29:32 -0600

    Updated StopAll Function

--- src/main/java/frc/robot/MoveDistanceCommand.java
+++ src/main/java/frc/robot/MoveDistanceCommand.java
@@ -38,11 +38,13 @@ public class MoveDistanceCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    driveSubsystem.genTankDrive();
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    driveSubsystem.stopAll();
   }
 
   // Returns true when the command should end.
