2202f0a2ac061ed9306e76b14904875af755803c
Space-Enthusiast-Nova <96958064+Space-Enthusiast-Nova@users.noreply.github.com>
2023-02-02 20:01:42 -0600

    Made it so the GrabOnCommand is toggleable and the LetGoCommand is hold-to-use

--- src/main/java/frc/robot/GrabOnCommand.java
+++ src/main/java/frc/robot/GrabOnCommand.java
@@ -31,9 +31,7 @@ public class GrabOnCommand extends CommandBase {
   
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-    GrabOnSubsystem.stopMotor();
-  }
+  public void end(boolean interrupted) {}
 
   
   // Returns true when the command should end.
