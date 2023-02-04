b0576f06d3cb5e9e69fac11d7bb081800f00eecd
Space-Enthusiast-Nova <96958064+Space-Enthusiast-Nova@users.noreply.github.com>
2023-02-04 14:48:21 -0600

    Update RotationCommand.java

--- src/main/java/frc/robot/RotationCommand.java
+++ src/main/java/frc/robot/RotationCommand.java
@@ -32,7 +32,7 @@ public class RotationCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    driveSubsystem.rotation(0.75);
+    driveSubsystem.rotation(MainConstants.RotationFactor);
   }
 
   // Called once the command ends or is interrupted.
