94427175ce203cce770de1b6a7edb7311f5f6f0b
Space-Enthusiast-Nova <96958064+Space-Enthusiast-Nova@users.noreply.github.com>
2023-02-04 09:52:01 -0600

    Update MainRobotContainer.java

--- src/main/java/frc/robot/MainRobotContainer.java
+++ src/main/java/frc/robot/MainRobotContainer.java
@@ -8,12 +8,13 @@ public class MainRobotContainer {
     private final Joystick m_stick = new Joystick(0);
     private final MainDriveSubsystem driveSubsystem = new MainDriveSubsystem();
     private final MainDriveCommand driveCommand = new MainDriveCommand(driveSubsystem, m_stick);
+    private final MainNavigationSubsystem navigationSubsystem = new MainNavigationSubsystem();
     private final ClawSubsystem GrabOnSubsystem = new ClawSubsystem();
     private final MainCamSubsystem camSubsystem = new MainCamSubsystem();
     private final GrabOnCommand grabOnCommand = new GrabOnCommand(GrabOnSubsystem, m_stick);
     private final LetGoCommand letGoCommand = new LetGoCommand(GrabOnSubsystem, m_stick);
     private final TurnToTagCommand turnToTagCommand = new TurnToTagCommand(driveSubsystem, camSubsystem);    
-    
+    private final RotationCommand rotationCommand = new RotationCommand(driveSubsystem, navigationSubsystem, 30);
     //Autonomous Section
     public Command getAutonomousCommand() {
     return grabOnCommand;
@@ -30,10 +31,12 @@ public class MainRobotContainer {
     JoystickButton button1 = new JoystickButton(m_stick, 1);
     JoystickButton button2 = new JoystickButton(m_stick, 2);
     JoystickButton button3 = new JoystickButton(m_stick, 3);
+    JoystickButton button4 = new JoystickButton(m_stick, 4);
     private void configureButtonBindings() {
         button1.whileTrue(grabOnCommand);
         button3.whileTrue(letGoCommand);
         button2.whileTrue(turnToTagCommand);
+        button4.whileTrue(rotationCommand);
     }
      
 }
