cf2557785e9b3b58f99c0434450c0bbbec68c554
Space-Enthusiast-Nova <96958064+Space-Enthusiast-Nova@users.noreply.github.com>
2023-02-04 11:16:19 -0600

    Updated StopAll function

--- src/main/java/frc/robot/MoveDistanceCommand.java
+++ src/main/java/frc/robot/MoveDistanceCommand.java
@@ -42,7 +42,7 @@ public class MoveDistanceCommand extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    driveSubsystem.stopAll();
+    driveSubsystem.stopAllDrive();
   }
 
   // Returns true when the command should end.
--- src/main/java/frc/robot/TurnToTagCommand.java
+++ src/main/java/frc/robot/TurnToTagCommand.java
@@ -33,7 +33,7 @@ public class TurnToTagCommand extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    driveSubsystem.stopAll();
+    driveSubsystem.stopAllDrive();
   }
 
   // Returns true when the command should end.
