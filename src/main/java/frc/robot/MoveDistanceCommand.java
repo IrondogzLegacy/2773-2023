9b5b2cc194eb1f315f207acb43f663c5b1de2075
Space-Enthusiast-Nova <96958064+Space-Enthusiast-Nova@users.noreply.github.com>
2023-02-04 10:06:16 -0600

    Create MoveDistanceCommand.java

---
+++ src/main/java/frc/robot/MoveDistanceCommand.java
@@ -0,0 +1,60 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class MoveDistanceCommand extends CommandBase {
+  /** Creates a new MoveDistanceCommand. */
+  private final MainNavigationSubsystem navigationSubsystem;
+  private final MainDriveSubsystem driveSubsystem;
+  double startDistance;
+  double stopDistance;
+  private final double distance;
+
+  public MoveDistanceCommand(MainNavigationSubsystem navigationSubsystem, MainDriveSubsystem driveSubsystem, double distance) {
+    this.navigationSubsystem = navigationSubsystem;
+    this.driveSubsystem = driveSubsystem;
+    this.distance = distance;
+    addRequirements(driveSubsystem);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    startDistance = navigationSubsystem.getDistance();
+
+      stopDistance = startDistance + distance;
+
+    System.out.print("Start" + startDistance);
+    System.out.println("Stop" + stopDistance);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    driveSubsystem.genTankDrive();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    driveSubsystem.stopAll();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    if (distance >= 0 && navigationSubsystem.getDistance() >= stopDistance) {
+      return true;
+    } 
+    if (distance <= 0 && navigationSubsystem.getDistance() <= stopDistance) {
+      return true;
+    } 
+    return false;
+
+  }
+}
