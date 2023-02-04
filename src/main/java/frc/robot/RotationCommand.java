1ba5ed1bdd9c8526e69deb9f92c43e652330717f
Space-Enthusiast-Nova <96958064+Space-Enthusiast-Nova@users.noreply.github.com>
2023-02-04 09:44:25 -0600

    Create RotationCommand.java

---
+++ src/main/java/frc/robot/RotationCommand.java
@@ -0,0 +1,53 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class RotationCommand extends CommandBase {
+  private final MainDriveSubsystem driveSubsystem;
+  private final MainNavigationSubsystem navigationSubsystem;
+  private final double turnAngle;
+  public RotationCommand(MainDriveSubsystem driveSubsystem, MainNavigationSubsystem navigationSubsystem, double turnAngle) {
+    this.driveSubsystem = driveSubsystem;
+    this.navigationSubsystem = navigationSubsystem;
+    this.turnAngle = turnAngle;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(driveSubsystem);
+  }
+
+  private double startAngle;
+  private double stopAngle;
+
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    startAngle = navigationSubsystem.getAngle();
+    stopAngle = startAngle + turnAngle;
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    driveSubsystem.rotation(0.75);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    driveSubsystem.stopAll();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    if(stopAngle <= navigationSubsystem.getAngle())
+    {
+      return true;
+    }
+    else return false;
+  }
+}
