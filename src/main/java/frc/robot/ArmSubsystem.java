db13f86f3f2b46a0551ac66b2846c1168450e8a1
Space-Enthusiast-Nova <96958064+Space-Enthusiast-Nova@users.noreply.github.com>
2023-02-14 18:46:10 -0600

    Found out how to find the built-in encoder for the arm

--- src/main/java/frc/robot/ArmSubsystem.java
+++ src/main/java/frc/robot/ArmSubsystem.java
@@ -5,12 +5,14 @@
 package frc.robot;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ArmSubsystem extends SubsystemBase {
 
   private final CANSparkMax ArmMotor = new CANSparkMax(MainConstants.ArmMotorCANID, MainConstants.motorType);
+  private final RelativeEncoder armEncoder = ArmMotor.getEncoder();
 
 public ArmSubsystem () {
   //unnecessary but I don't care
