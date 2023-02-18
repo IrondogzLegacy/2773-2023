a54c738ce2e6070fcb2c6f01b7008332ad0232ae
Tacocat1016 <122695971+Tacocat1016@users.noreply.github.com>
2023-02-18 13:28:08 -0600

    Update MainConstants.java

--- src/main/java/frc/robot/MainConstants.java
+++ src/main/java/frc/robot/MainConstants.java
@@ -32,7 +32,7 @@ public class MainConstants {
     //constant for the ArmMotor
     public static final int ArmMotorCANID = 17;
     // constant for ArmMotor Speed
-    public static final double ArmMotorSpeed = 0.5;
+    public static final double ArmMotorSpeed = 0.3;
     public static final double ArmEncoderRatio = 0.04*0.75*3.14;
 
     public static final double BalanceSpeed = 0.2;
