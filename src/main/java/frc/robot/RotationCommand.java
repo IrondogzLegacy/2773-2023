dbd5525f37ade323c959629ec1e145296e6379ac
IronDogz <finndlacher@gmail.com>
2023-02-23 19:14:46 -0600

    Added a few comments to Rotation Command

--- src/main/java/frc/robot/RotationCommand.java
+++ src/main/java/frc/robot/RotationCommand.java
@@ -32,26 +32,37 @@ public class RotationCommand extends CommandBase {
   public void initialize() {
     startAngle = navigationSubsystem.getAngle();
     stopAngle = startAngle + turnAngle;
+    //Goal = Start + Action
     pid.setSetpoint(stopAngle);
     pid.setTolerance(2);
     //System.out.println(startAngle);
     //System.out.println(stopAngle);
+   
+    /*Above lines are commented due to them not being needed. 
+    //Tolerance is the difference between what the actual angle is and what we want it to be.
+    //If the robot turns 92 degrees instead of 90, its probaly fine. Tolerance should be lowered
+    //If we need to be more precise. 
+    */
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     var speed = pid.calculate(navigationSubsystem.getAngle());
+    //Speed is based on angle
     //System.out.println(navigationSubsystem.getAngle());
     speed = Math.min(speed, MainConstants.MaxRotationSpeed);
     speed = Math.max(speed,-MainConstants.MaxRotationSpeed);
+    //Speed is the range of rotationspeed.
     driveSubsystem.rotation(speed);
+    //rotate
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     driveSubsystem.stopAllDrive();
+    //stop
   }
 
   // Returns true when the command should end.
