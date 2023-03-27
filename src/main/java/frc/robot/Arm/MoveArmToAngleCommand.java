dcd567ff2edb9e73f8c45dae99f1101e6c368d4c
Space-Enthusiast-Nova <96958064+Space-Enthusiast-Nova@users.noreply.github.com>
2023-03-27 18:24:57 -0500

    Added PID controller for rotation

--- src/main/java/frc/robot/Arm/MoveArmToAngleCommand.java
+++ src/main/java/frc/robot/Arm/MoveArmToAngleCommand.java
@@ -4,39 +4,37 @@
 
 package frc.robot.Arm;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class ReturnArmTo0Command extends CommandBase {
+public class MoveArmToAngleCommand extends CommandBase {
   /** Creates a new ReturnArmTo0. */
   private ArmSubsystem armSubsystem;
-  public ReturnArmTo0Command(ArmSubsystem armSubsystem) {
+  private PIDController rotateAnglePID = new PIDController(0.01, 0, 0);
+  private double endAngle;
+
+
+  public MoveArmToAngleCommand(ArmSubsystem armSubsystem, double endAngle) {
     this.armSubsystem = armSubsystem;
     addRequirements(armSubsystem);
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
-  private double startingArmAngle;
   private double currentArmAngle;
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    startingArmAngle = armSubsystem.getRotationAngle();
+    currentArmAngle = armSubsystem.getRotationAngle();
+    rotateAnglePID.setSetpoint(endAngle);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    currentArmAngle = armSubsystem.getRotationAngle();
-    if (startingArmAngle < -1) {
-      while (currentArmAngle < -1)
-      {armSubsystem.rotateUp();}
-      armSubsystem.rotateStop();
-    }
-    if (startingArmAngle > 1) {
-      while (currentArmAngle > 1)
-      {armSubsystem.rotateDown();}
-      armSubsystem.rotateStop();
-    }
+    double rotateSpeed = rotateAnglePID.calculate(armSubsystem.getRotationAngle());
+      rotateSpeed = MathUtil.clamp(rotateSpeed, -0.2, 0.2);
+      armSubsystem.rotate(rotateSpeed);
   }
 
   // Called once the command ends or is interrupted.
