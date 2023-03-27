de3ffab5d6ffdc52b32850789a78a7613d350702
Space-Enthusiast-Nova <96958064+Space-Enthusiast-Nova@users.noreply.github.com>
2023-03-27 18:46:18 -0500

    Update MainRobot.java

--- src/main/java/frc/robot/MainRobot.java
+++ src/main/java/frc/robot/MainRobot.java
@@ -4,7 +4,11 @@
 
 package frc.robot;
 
+import java.nio.LongBuffer;
+
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.TimedRobot;
 //import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -16,12 +20,33 @@ public class MainRobot extends TimedRobot {
 
   MainRobotContainer robotContainer;
   Command autonomousCommand;
-
-  @Override
+  AddressableLED m_led = new AddressableLED(9);
+    AddressableLEDBuffer m_ledBuffer;
+  
+    @Override
   public void robotInit() {
     robotContainer = new MainRobotContainer();
     robotContainer.resetGyro();
     CameraServer.startAutomaticCapture();
+    // PWM port 9
+    // Must be a PWM header, not MXP or DIO
+    
+for (var i = 0; i < m_ledBuffer.getLength(); i++) {
+   // Sets the specified LED to the RGB values for red
+   m_ledBuffer.setRGB(i, 255, 0, 0);
+
+
+m_led.setData(m_ledBuffer);
+    // Reuse buffer
+    // Default to a length of 60, start empty output
+    // Length is expensive to set, so only set it once, then just update data
+    m_ledBuffer = new AddressableLEDBuffer(60);
+    m_led.setLength(m_ledBuffer.getLength());
+
+    // Set the data
+    m_led.setData(m_ledBuffer);
+    m_led.start();
+  }
   }
 
   @Override
